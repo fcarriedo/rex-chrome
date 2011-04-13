04d9620cef291201ef2ff65a6b1214623e52afa8
Francisco Carriedo <fcarriedo@gmail.com>
2011-04-13 12:35:22 -0700

    Refactored and documented some code.

--- resources/js/rex.js
+++ resources/js/rex.js
@@ -5,6 +5,48 @@ function initRex() {
   checkCredentials();
 }
 
+
+/** ======================================
+ *   Convore API
+ *    TODO: (Put this into a separate lib)
+ *  ======================================
+ */
+function fetchGroups( callback ) {
+  var url = convoreApiUrl + '/groups.json';
+  $.getJSON(url, function(data) {
+    callback.call(this, data.groups );
+  });
+}
+
+function fetchTopics(groupId, callback) {
+  var url = convoreApiUrl + '/groups/' + groupId + '/topics.json';
+  $.getJSON(url, function(data) {
+    callback.call(this, data.topics );
+  });
+}
+
+function fetchMessages(topicId, callback) {
+  var url = convoreApiUrl + '/topics/' + topicId + '/messages.json';
+  $.getJSON(url, function(data) {
+    callback.call(this, data.messages );
+  });
+}
+
+/** ======================================
+ *   Convore chrome utils
+ *  ======================================
+ */
+function openInConvore(relativePath) {
+  var url = convoreUrl + relativePath;
+  chrome.tabs.create({url: url}, function(tab) {});
+}
+
+
+/** ======================================
+ *   Authentication
+ *  ======================================
+ */
+
 function checkCredentials() {
   var popupView = getPopupView();
   if(popupView) {
@@ -16,42 +58,6 @@ function checkCredentials() {
     }
   }
 }
-
-function setupGlobalCreds(convoreUsr, convorePswd) {
-  // Set usr/pswd for general ajax calls.
-  $.ajaxSetup({
-    username: convoreUsr,
-    password: convorePswd
-  });
-
-  // Persist creds in the localStorage
-  persistCredentials(convoreUsr, convorePswd);
-}
-
-
-function retrieveCredentials() {
-  var auth = localStorage.getItem('convore.auth');
-  if( !auth ) {
-    var popupView = getPopupView();
-    if( popupView ) {
-      popupView.showLoginPage();
-    }
-  } else {
-    var authParts = auth.split(':');
-    var usr = authParts[0];
-    var pswd = authParts[1];
-
-    var authContext = {
-      username: usr,
-      password: pswd,
-      success: null,
-      error: null
-    };
-
-    return authContext;
-  }
-}
-
 
 function validateCredentials(authContext) {
   var verifyAccountUrl = convoreApiUrl + '/account/verify.json';
@@ -81,25 +87,73 @@ function validateCredentials(authContext) {
   });
 }
 
-function fetchGroups( callback ) {
-  var url = convoreApiUrl + '/groups.json';
-  $.getJSON(url, function(data) {
-    callback.call(this, data.groups );
-  });
-}
+function retrieveCredentials() {
+  var auth = datastore.get('convore.auth');
+  if( !auth ) {
+    var popupView = getPopupView();
+    if( popupView ) {
+      popupView.showLoginPage();
+    }
+  } else {
+    var authParts = auth.split(':');
+    var usr = authParts[0];
+    var pswd = authParts[1];
 
-function openInConvore(relativePath) {
-  var url = convoreUrl + relativePath;
-  chrome.tabs.create({url: url}, function(tab) {});
+    var authContext = {
+      username: usr,
+      password: pswd,
+      success: null,
+      error: null
+    };
+
+    return authContext;
+  }
 }
 
 function removeCredentials() {
-  localStorage.removeItem('convore.auth');
+  datastore.delete('convore.auth');
 }
 
 function persistCredentials(username, password) {
-  localStorage.setItem('convore.auth', username + ':' + password);
+  datastore.save('convore.auth', username + ':' + password);
 }
+
+function setupGlobalCreds(convoreUsr, convorePswd) {
+  // Set usr/pswd for general ajax calls.
+  $.ajaxSetup({
+    username: convoreUsr,
+    password: convorePswd
+  });
+
+  // Persist creds in the localStorage
+  persistCredentials(convoreUsr, convorePswd);
+}
+
+/** ======================================
+ *   Persistence abstraction
+*    (implemented on localStorage)
+ *  ======================================
+ */
+
+var datastore = function(storage) {
+  return {
+    get: function(key) {
+      return storage.getItem(key);
+    },
+    save: function(key, value) {
+      storage.setItem(key, value);
+    },
+    delete: function(key) {
+      storage.removeItem(key);
+    }
+  };
+}(localStorage);
+
+
+/** ======================================
+ *   Chrome extension utils
+ *  ======================================
+ */
 
 function getPopupView() {
   return chrome.extension.getViews({type:'popup'})[0];
